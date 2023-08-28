e0393f35148d47cf68e06a7f8e13b397193eda28
dudushy <eduardotalarico1337@gmail.com>
2023-08-28 02:36:59 -0300

    Fix bugs + rename repo + clockArray (WIP)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# [TimeAgoDisplay](https://github.com/dudushy/TimeAgoDisplay/)
+# [TimeDisplay](https://github.com/dudushy/TimeDisplay/)
 A website that calculates and visually presents the time elapsed since a given date in a human-readable format, using years, weeks, days, hours, minutes, and seconds.
--- docs/script.js
+++ docs/script.js
@@ -1,53 +1,86 @@
-const TITLE = "TimeAgoDisplay"
+const TITLE = "TimeDisplay";
+const clockArray = [];
 
-console.log(`[${TITLE}] Hello World!`);
+function calculateTimeDifference(dateInput, sample = false) {
+  console.log(`[${TITLE}#calculateTimeDifference] dateInput`, dateInput);
 
-function calculateTimeDifference() {
-  const dateInput = document.getElementById("dateInput").value;
-  const selectedDate = new Date(dateInput).toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });;
-  console.log(`[${TITLE}] selectedDate`, selectedDate);
+  if (sample) {
+    dateInput = document.getElementById("sampleDateInput").value;
+    console.log(`[${TITLE}#calculateTimeDifference] (sample) dateInput`, dateInput);
+  }
 
-  const currentDate = new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });;
-  console.log(`[${TITLE}] currentDate`, currentDate);
+  if (!dateInput) return null;
 
-  const timeDifference = currentDate - selectedDate;
-  console.log(`[${TITLE}] timeDifference`, timeDifference);
+  const selectedDate = new Date(`${dateInput}T00:00:00`);
+  console.log(`[${TITLE}#calculateTimeDifference] selectedDate`, selectedDate);
 
-  const seconds = Math.floor(timeDifference / 1000);
-  console.log(`[${TITLE}] seconds`, seconds);
+  const currentDate = new Date();
+  console.log(`[${TITLE}#calculateTimeDifference] currentDate`, currentDate);
+
+  const timeDifference = selectedDate - currentDate;
+  console.log(`[${TITLE}#calculateTimeDifference] timeDifference`, timeDifference);
+
+  const seconds = Math.abs(Math.floor(timeDifference / 1000));
+  console.log(`[${TITLE}#calculateTimeDifference] seconds`, seconds);
 
   const minutes = Math.floor(seconds / 60);
-  console.log(`[${TITLE}] minutes`, minutes);
+  console.log(`[${TITLE}#calculateTimeDifference] minutes`, minutes);
 
   const hours = Math.floor(minutes / 60);
-  console.log(`[${TITLE}] hours`, hours);
+  console.log(`[${TITLE}#calculateTimeDifference] hours`, hours);
 
   const days = Math.floor(hours / 24);
-  console.log(`[${TITLE}] days`, days);
+  console.log(`[${TITLE}#calculateTimeDifference] days`, days);
 
   const weeks = Math.floor(days / 7);
-  console.log(`[${TITLE}] weeks`, weeks);
+  console.log(`[${TITLE}#calculateTimeDifference] weeks`, weeks);
 
   const years = Math.floor(weeks / 52);
-  console.log(`[${TITLE}] years`, years);
+  console.log(`[${TITLE}#calculateTimeDifference] years`, years);
 
   const remainingWeeks = weeks % 52;
-  console.log(`[${TITLE}] remainingWeeks`, remainingWeeks);
+  console.log(`[${TITLE}#calculateTimeDifference] remainingWeeks`, remainingWeeks);
 
   const remainingDays = days % 7;
-  console.log(`[${TITLE}] remainingDays`, remainingDays);
+  console.log(`[${TITLE}#calculateTimeDifference] remainingDays`, remainingDays);
 
   const remainingHours = hours % 24;
-  console.log(`[${TITLE}] remainingHours`, remainingHours);
+  console.log(`[${TITLE}#calculateTimeDifference] remainingHours`, remainingHours);
 
   const remainingMinutes = minutes % 60;
-  console.log(`[${TITLE}] remainingMinutes`, remainingMinutes);
+  console.log(`[${TITLE}#calculateTimeDifference] remainingMinutes`, remainingMinutes);
 
   const remainingSeconds = seconds % 60;
-  console.log(`[${TITLE}] remainingSeconds`, remainingSeconds);
+  console.log(`[${TITLE}#calculateTimeDifference] remainingSeconds`, remainingSeconds);
 
-  const resultString = `${years}y ${remainingWeeks}w ${remainingDays}d ${remainingHours}h ${remainingMinutes}min ${remainingSeconds}s`;
-  console.log(`[${TITLE}] resultString`, resultString);
+  let resultTime = `${years}y ${remainingWeeks}w ${remainingDays}d ${remainingHours}h ${remainingMinutes}min ${remainingSeconds}s`;
+  console.log(`[${TITLE}#calculateTimeDifference] (BEFORE) resultTime`, resultTime);
 
-  document.getElementById("output").textContent = resultString;
+  if (timeDifference <= 0) resultTime += " ago";
+  console.log(`[${TITLE}#calculateTimeDifference] (AFTER) resultTime`, resultTime);
+
+  if (sample) document.getElementById("output").textContent = resultTime;
+  else return resultTime;
+}
+
+function addClock() {
+  if (!document.getElementById("sampleDateInput").value) {
+    alert("Please enter a date");
+    return;
+  }
+
+  console.log(`[${TITLE}#addClock] clockArray`, clockArray);
+
+  // clockInput.setAttribute("onchange", "calculateTimeDifference(this.value)");
+  // clockArray.push(clock);
+
+  const clock = document.createElement("p");
+  setInterval(test, 1000, clock, "a")
+  document.getElementById("clocks").appendChild(clock);
+}
+
+function test(clock, char) {
+  console.log(`[${TITLE}#test] (${char}) clock`, clock);
+
+  clock.innerText += char;
 }
